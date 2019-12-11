ae902465ca8b16718da0ed5764725a984bf36f78
James Goodall <jfgoodall@users.noreply.github.com>
2019-12-11 13:57:52 -0700

    day 9 solution

---
+++ 2019/py/day9.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+from intcode_cpu import IntcodeCPU
+
+cpu = IntcodeCPU()
+
+example_mem = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+assert cpu.run_program(example_mem) == example_mem
+
+example_mem = [1102,34915192,34915192,7,4,7,99,0]
+assert cpu.run_program(example_mem) == [1219070632396864]
+
+example_mem = [104,1125899906842624,99]
+assert cpu.run_program(example_mem) == [example_mem[1]]
+
+with open('day9.dat') as f:
+    memory = f.readlines()
+    assert len(memory) == 1
+    memory = map(int, memory[0].strip().split(','))
+
+print(cpu.run_program(memory, [1]))
+print(cpu.run_program(memory, [2]))
+
--- 2019/py/intcode_cpu.py
+++ 2019/py/intcode_cpu.py
@@ -1,7 +1,13 @@
 from __future__ import division
+from enum import Enum
 
 
 class IntcodeCPU(object):
+    class ParameterMode(Enum):
+        POSITION = 0
+        IMMEDIATE = 1
+        RELATIVE = 2
+
     def __init__(self):
         self._instruction_map = {
             1: self._execute_instr_ADD,
@@ -12,29 +18,37 @@ class IntcodeCPU(object):
             6: self._execute_instr_JIF,
             7: self._execute_instr_LT,
             8: self._execute_instr_EQU,
+            9: self._execute_instr_ARB,
             99: self._execute_instr_HCF,
         }
 
     def _get_parameter_mode(self, param):
-        return self._get_opcode() // 10**(param+1) % 10
+        return self.ParameterMode(self._get_opcode() // 10**(param+1) % 10)
+
+    def _get_param_val_ptr(self, param):
+        parameter_mode = self._get_parameter_mode(param)
+        if parameter_mode == self.ParameterMode.POSITION:
+            addr = self._memory[self._pc+param]
+        elif parameter_mode == self.ParameterMode.IMMEDIATE:
+            addr = self._pc + param
+        elif parameter_mode == self.ParameterMode.RELATIVE:
+            addr = + self._memory[self._pc+param] + self._relative_base_offset
+        else:
+            raise Exception("unknown parameter mode: {}".format(parameter_mode))
+
+        assert addr >= 0
+        if len(self._memory) <= addr:
+            extension = addr - len(self._memory) + 1
+            self._memory.extend([0]*extension)
+        return addr
 
     def _get_param(self, param):
-        parameter_mode = self._get_parameter_mode(param)
-        if parameter_mode == 0:
-            return self._memory[self._memory[self._pc+param]]
-        elif parameter_mode == 1:
-            return self._memory[self._pc+param]
-        else:
-            raise Exception("unknown parameter mode: {}".format(parameter_mode))
+        addr = self._get_param_val_ptr(param)
+        return self._memory[addr]
 
     def _set_param(self, param, val):
-        parameter_mode = self._get_parameter_mode(param)
-        if parameter_mode == 0:
-            self._memory[self._memory[self._pc+param]] = val
-        elif parameter_mode == 1:
-            raise Exception("illegal parameter mode: {}".format(parameter_mode))
-        else:
-            raise Exception("unknown parameter mode: {}".format(parameter_mode))
+        addr = self._get_param_val_ptr(param)
+        self._memory[addr] = val
 
     def _get_opcode(self):
         return self._memory[self._pc]
@@ -90,6 +104,11 @@ class IntcodeCPU(object):
             self._set_param(3, 0)
         self._advance_program_counter(4)
 
+    def _execute_instr_ARB(self):
+        self._relative_base_offset += self._get_param(1)
+        p = self._get_param(1)
+        self._advance_program_counter(2)
+
     def _execute_instr_HCF(self):
         self._halted = True
         self._break = True
@@ -108,16 +127,21 @@ class IntcodeCPU(object):
 
     def load_program(self, memory):
         self._pc = 0
+        self._relative_base_offset = 0
         self._halted = False
         self._break = True
         self._memory = list(memory)
 
-    def run_program(self, memory, in_data, debug=False):
+    def run_program(self, memory, in_data=None, debug=False):
+        if in_data is None:
+            in_data = []
         self.load_program(memory)
         self._run(in_data, debug=debug)
         return self._out_data
 
-    def continue_program(self, in_data, debug=False):
+    def continue_program(self, in_data=None, debug=False):
+        if in_data is None:
+            in_data = []
         self._run(in_data, debug=debug)
         return self._out_data
 
