ad965bbc0ac5e3c2f3ceac9a4c5f0e0a73cb9857
James Goodall <jfgoodall@users.noreply.github.com>
2019-12-13 10:30:01 -0700

    day 13 solution

---
+++ 2019/py/day13.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+from intcode_cpu import IntcodeCPU
+from enum import Enum
+import itertools
+
+class Tile(Enum):
+    Empty = 0
+    Wall = 1
+    Block = 2
+    Paddle = 3
+    Ball = 4
+
+with open('day13.dat') as f:
+    memory = f.readlines()
+    assert len(memory) == 1
+    memory = map(int, memory[0].strip().split(','))
+
+
+cpu = IntcodeCPU()
+output = cpu.run_program(memory)
+block_tiles = [t for t in output[2::3] if Tile(t) == Tile.Block]
+assert len(block_tiles) == 348
+print("part 1: {}".format(len(block_tiles)))
+
+
+memory[0] = 2
+cpu.load_program(memory)
+in_data = []
+score = 0
+while not cpu.halted():
+    output = cpu.continue_program(in_data)
+    iterators = [iter(output)] * 3
+    for x, y, tile in itertools.izip_longest(*iterators):
+        if (x, y) == (-1, 0):
+            score = tile
+        elif Tile(tile) == Tile.Ball:
+            ball_x = x
+        elif Tile(tile) == Tile.Paddle:
+            paddle_x = x
+    if ball_x < paddle_x:
+        in_data = [-1]
+    elif ball_x > paddle_x:
+        in_data = [1]
+    else:
+        in_data = [0]
+
+assert score == 16999
+print("part 2: {}".format(score))
+
