5ce3f0e136d1d0bce24f86a9aa5360f98ae421b8
James Goodall <jfgoodall@users.noreply.github.com>
2021-12-12 11:46:41 -0700

    always tweaking

--- 2021/py/template.py
+++ 2021/py/template.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python3
-from io import StringIO
+import itertools, functools
 import numpy as np
+from io import StringIO
+from collections import Counter, defaultdict
 try:
     from tqdm import tqdm
 except ImportError:
     def tqdm(iterable=None, **kwargs):
         return iterable
 
-def part1():
+def part1(parsed):
     pass
 
-def part2():
+def part2(parsed):
     pass
 
 def parse_input(data_src):
-    inp = data_src.readlines()
-
+    data_src.seek(0)
+    for line in data_src:
+        pass
     return parsed
 
 def run_tests():
     TEST_INPUT = """
-test
-data
+test data
 """
     with StringIO(TEST_INPUT.strip()) as test_data:
-        parsed = parse_input(test_data)
-    # assert part1(parsed) == 165
+        assert part1(parse_input(test_data)) == 0
+        # assert part2(parse_input(test_data)) == 0
 
 if __name__ == '__main__':
     run_tests()
-    with open(__file__[:-3] + '-input.dat') as infile:
-        parsed = parse_input(infile)
-    print(f"Part 1: {part1(parsed)}")  # <ans>
-    print(f"Part 2: {part2(parsed)}")  # <ans>
+    # with open(__file__[:-3] + '-input.dat') as infile:
+    #     print(f"Part 1: {part1(parse_input(infile))}")  # 4411
+    #     print(f"Part 2: {part2(parse_input(infile))}")  # 136767
