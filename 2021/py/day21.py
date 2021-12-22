004007ccd8fc9d1ad84ca027554b0c2b787b3935
James Goodall <jfgoodall@users.noreply.github.com>
2021-12-22 10:18:08 -0700

    day 21

--- 2021/py/day21.py
+++ 2021/py/day21.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import time, itertools, functools, re, random
+import time, itertools, functools, re
 import numpy as np
 from io import StringIO
 from collections import Counter, defaultdict
@@ -10,57 +10,48 @@ except ImportError:
     def tqdm(iterable=None, **kwargs):
         return iterable
 
-@dataclass
+@dataclass(frozen=True)
 class Player:
     pos: int = 0
     score: int = 0
     turns: int = 0
-
-class Node:
-    __slots__ = 'pos', 'score', 'left', 'center', 'right'
-    def __init__(self, pos):
-        self.pos = pos
-        self.score = 0
-        self.left = None
-        self.center = None
-        self.right = None
 
 def part1(p1_pos, p2_pos):
     p1, p2 = Player(pos=p1_pos), Player(pos=p2_pos)
     die = itertools.cycle(range(1, 101))
 
     while True:
-        roll = sum(next(die) for _ in range(3))
-        p1.pos = (p1.pos+roll-1)%10 + 1
-        p1.score += p1.pos
-        p1.turns += 1
+        roll_total = sum(itertools.islice(die, 3))
+        new_pos = (p1.pos+roll_total-1) % 10 + 1
+        p1 = Player(new_pos, p1.score+new_pos, p1.turns+1)
         if p1.score >= 1000:
             break
         p1, p2 = p2, p1
 
     return p2.score * (p1.turns+p2.turns)*3
 
+ROLLS = Counter(sum(roll) for roll in itertools.product(range(1, 4), repeat=3))
+
+@functools.lru_cache(maxsize=None)
+def count_wins(p1, p2):
+    wins = np.zeros(2, dtype=int)
+    for roll_total, freq in ROLLS.items():
+        new_pos = (p1.pos+roll_total-1) % 10 + 1
+        new_score = p1.score - new_pos
+        if new_score <= 0:
+            wins[0] += freq
+        else:
+            wins += freq * count_wins(p2, Player(new_pos, new_score))[::-1]
+    return wins
+
 def part2(p1_pos, p2_pos):
-    root = Node(p1_pos)
-    leaves = 0
-    def build_tree(node, depth=0):
-        print(f'depth: {depth}, pos: {node.pos}, score: {node.score}')
-        for val, child in enumerate((node.left, node.center, node.right), 1):
-            child = Node((node.score+val-1)%10+1)
-            child.score = node.score + child.pos
-            if child.score < 21:
-                build_tree(child, depth+1)
-            else:
-                nonlocal leaves
-                leaves += 1
-    build_tree(root)
-    print("leaves:", leaves)
+    return max(count_wins(Player(p1_pos, 21), Player(p2_pos, 21)))
 
 def parse_input(data_src):
     data_src.seek(0)
-    p1 = int(next(data_src).split()[-1])
-    p2 = int(next(data_src).split()[-1])
-    return p1, p2
+    p1_pos = int(next(data_src).split()[-1])
+    p2_pos = int(next(data_src).split()[-1])
+    return p1_pos, p2_pos
 
 def run_tests():
     TEST_INPUT = """
@@ -80,5 +71,5 @@ def print_result(part_label, part_fn, *args):
 if __name__ == '__main__':
     run_tests()
     with open(__file__[:-3] + '-input.dat') as infile:
-        print_result('1', part1, *parse_input(infile))  # -
-    #     print_result('2', part2, *parse_input(infile))  # -
+        print_result('1', part1, *parse_input(infile))  # 513936
+        print_result('2', part2, *parse_input(infile))  # 105619718613031
