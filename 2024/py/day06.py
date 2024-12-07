200e2e2068be4c8a34a8ba6b3e7cb33a0021b4bc
James Goodall <jfgoodall@users.noreply.github.com>
2024-12-06 23:09:09 -0700

    day 6, 7

---
+++ 2024/py/day06.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+import functools
+import itertools
+import math
+import os
+import re
+import sys
+import time
+import typing
+from collections import Counter, defaultdict, namedtuple
+from dataclasses import dataclass
+from enum import Enum, IntEnum
+from functools import lru_cache
+from io import StringIO
+from pprint import pprint
+
+import numpy as np
+
+def find_starting_location(grid):
+    vec = None
+    for r, row in enumerate(grid):
+        for c, cell in enumerate(list(row)):
+            match cell:
+                case '^':
+                    vec = (-1, 0)
+                case 'v':
+                    vec = (1, 0)
+                case '<':
+                    vec = (0, -1)
+                case '>':
+                    vec = (0, 1)
+                case _:
+                    continue
+            pos = (r, c)
+            break
+        if vec: break
+
+    return pos, vec
+
+def walk_patrol(grid, pos, vec, obstacle=None):
+    ROWS = len(grid)
+    COLS = len(grid[0])
+    
+    visited = {((pos, vec),)}
+    while True:
+        next = pos[0]+vec[0], pos[1]+vec[1]
+        if 0 <= next[0] < ROWS and 0 <= next[1] < COLS:
+            if grid[next[0]][next[1]] == '#' or next == obstacle:
+                vec = vec[1], -vec[0]  # turn right
+            else:
+                pos = next
+                if (pos, vec) in visited:
+                    return None  # patrol is a loop
+                visited.add((pos, vec))
+        else:
+            break
+
+    return {v[0] for v in visited}
+
+def part1(grid):
+    pos, vec = find_starting_location(grid)
+    visited = walk_patrol(grid, pos, vec)
+    return len(visited)
+
+def part2(grid):
+    pos, vec = find_starting_location(grid)
+    obstacles = walk_patrol(grid, pos, vec)
+    obstacles.remove(pos)
+
+    loops = 0
+    for obstacle in obstacles:
+        visited = walk_patrol(grid, pos, vec, obstacle)
+        if visited is None:
+            loops += 1
+
+    return loops
+
+def parse_input(data_src: typing.TextIO) -> list[typing.Any]:
+    data_src.seek(0)
+    return [data_src.read().splitlines()]
+
+def main():
+    (test1_data, test1_answer), (test2_data, test2_answer) = get_test_data()
+    with open(__file__[:-3] + '-input.dat') as infile:
+        my_part1_answer = part1(*parse_input(test1_data)) 
+        assert my_part1_answer == test1_answer, f"got {my_part1_answer}; should be {test1_answer}"
+        solve_part('1', part1, *parse_input(infile))  # 5086
+
+        my_part2_answer = part2(*parse_input(test2_data)) 
+        assert my_part2_answer == test2_answer, f"got {my_part2_answer}; should be {test2_answer}"
+        solve_part('2', part2, *parse_input(infile))  # 1770
+
+def solve_part(part_label: str, part_fn: typing.Callable, *args):
+    start = time.perf_counter()
+    result = part_fn(*args)
+    end = time.perf_counter()
+    print(f"Part {part_label}: {result}  ({int((end-start)*1000)} ms)")
+
+def get_test_data() -> tuple[tuple[str, str|float], tuple[str, str|float]]:
+    """Keep test data out of the way at the bottom of this file."""
+    TEST_INPUT1 = """
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+"""
+    TEST_ANSWER1 = 41
+
+    TEST_INPUT2 = TEST_INPUT1
+    TEST_ANSWER2 = 6
+
+    return (
+        (StringIO(TEST_INPUT1.strip()), TEST_ANSWER1),
+        (StringIO(TEST_INPUT2.strip()), TEST_ANSWER2)
+    )
+
+if __name__ == '__main__':
+    main()
---
+++ 2024/py/day07.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+import functools
+import itertools
+import math
+import os
+import re
+import sys
+import time
+import typing
+from collections import Counter, defaultdict, namedtuple
+from dataclasses import dataclass
+from enum import Enum, IntEnum
+from functools import lru_cache
+from io import StringIO
+from pprint import pprint
+
+import numpy as np
+
+def part1(data):
+    answer = 0
+    for total, operands in data:
+        ops_possibilities = itertools.product('+*', repeat=len(operands)-1)
+        for ops_combo in ops_possibilities:
+            res = operands[0]
+            for operator, operand in zip(ops_combo, operands[1:]):
+                match operator:
+                    case '+':
+                        res += operand
+                    case '*':
+                        res *= operand
+                if res > total: break
+            if res == total:
+                answer += total
+                break
+
+    return answer
+
+def part2(data):
+    answer = 0
+    for total, operands in data:
+        ops_possibilities = itertools.product('+*|', repeat=len(operands)-1)
+        for ops_combo in ops_possibilities:
+            res = operands[0]
+            for operator, operand in zip(ops_combo, operands[1:]):
+                match operator:
+                    case '+':
+                        res += operand
+                    case '*':
+                        res *= operand
+                    case '|':
+                        res = int(str(res) + str(operand))
+                if res > total: break
+            if res == total:
+                answer += total
+                break
+
+    return answer
+
+def parse_input(data_src: typing.TextIO) -> list[typing.Any]:
+    data_src.seek(0)
+    data = []
+    for line in data_src.read().splitlines():
+        total, operands = line.split(':')
+        operands = operands.split()
+        data.append((int(total), tuple(map(int, operands))))
+    return [data]
+
+def main():
+    (test1_data, test1_answer), (test2_data, test2_answer) = get_test_data()
+    with open(__file__[:-3] + '-input.dat') as infile:
+        my_part1_answer = part1(*parse_input(test1_data)) 
+        assert my_part1_answer == test1_answer, f"got {my_part1_answer}; should be {test1_answer}"
+        solve_part('1', part1, *parse_input(infile))  # 7710205485870
+
+        my_part2_answer = part2(*parse_input(test2_data)) 
+        assert my_part2_answer == test2_answer, f"got {my_part2_answer}; should be {test2_answer}"
+        solve_part('2', part2, *parse_input(infile))  # 20928985450275
+
+def solve_part(part_label: str, part_fn: typing.Callable, *args):
+    start = time.perf_counter()
+    result = part_fn(*args)
+    end = time.perf_counter()
+    print(f"Part {part_label}: {result}  ({int((end-start)*1000)} ms)")
+
+def get_test_data() -> tuple[tuple[str, str|float], tuple[str, str|float]]:
+    """Keep test data out of the way at the bottom of this file."""
+    TEST_INPUT1 = """
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+"""
+    TEST_ANSWER1 = 3749
+
+    TEST_INPUT2 = TEST_INPUT1
+    TEST_ANSWER2 = 11387
+
+    return (
+        (StringIO(TEST_INPUT1.strip()), TEST_ANSWER1),
+        (StringIO(TEST_INPUT2.strip()), TEST_ANSWER2)
+    )
+
+if __name__ == '__main__':
+    main()
